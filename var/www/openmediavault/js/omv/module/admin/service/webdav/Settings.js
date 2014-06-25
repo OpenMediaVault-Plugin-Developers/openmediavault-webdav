2478111b183d337c3bd2a8afdbd21b88b2d82b3f
Aaron Murray <plugins@omv-extras.org>
2014-06-25 07:45:46 -0500

    update locales

--- var/www/openmediavault/js/omv/module/admin/service/webdav/Settings.js
+++ var/www/openmediavault/js/omv/module/admin/service/webdav/Settings.js
@@ -1,22 +1,22 @@
 /**
- * This file is part of OpenMediaVault.
- *
  * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
- * @author    Aaron Murray <aaron@omv-extras.org>
- * @copyright Copyright (c) 2013 Aaron Murray
+ * @author    Volker Theile <volker.theile@openmediavault.org>
+ * @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
+ * @copyright Copyright (c) 2009-2013 Volker Theile
+ * @copyright Copyright (c) 2013-2014 OpenMediaVault Plugin Developers
  *
- * OpenMediaVault is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * OpenMediaVault is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 // require("js/omv/WorkspaceManager.js")
 // require("js/omv/workspace/form/Panel.js")
@@ -27,36 +27,36 @@
  * @derived OMV.workspace.form.Panel
  */
 Ext.define("OMV.module.admin.service.webdav.Settings", {
-    extend: "OMV.workspace.form.Panel",
-    uses: [
+    extend : "OMV.workspace.form.Panel",
+    uses   : [
         "OMV.form.field.SharedFolderComboBox"
     ],
 
-    rpcService: "WebDav",
-    rpcGetMethod: "getSettings",
-    rpcSetMethod: "setSettings",
+    rpcService   : "WebDav",
+    rpcGetMethod : "getSettings",
+    rpcSetMethod : "setSettings",
 
-    getFormItems: function() {
+    getFormItems : function() {
         var me = this;
         return [{
-            xtype: "fieldset",
-            title: _("General settings"),
-            fieldDefaults: {
-                labelSeparator: ""
+            xtype         : "fieldset",
+            title         : _("General settings"),
+            fieldDefaults : {
+                labelSeparator : ""
             },
-            items: [{
-                xtype: "checkbox",
-                name: "enable",
-                fieldLabel: _("Enable"),
-                checked: false
+            items         : [{
+                xtype      : "checkbox",
+                name       : "enable",
+                fieldLabel : _("Enable"),
+                checked    : false
             },{
-                xtype: "sharedfoldercombo",
-                name: "sharedfolderref",
-                fieldLabel: _("Shared Folder"),
-                allowNone: false,
-                plugins: [{
-                    ptype: "fieldinfo",
-                    text: _("This share will have ownership changed to www-data:users.")
+                xtype      : "sharedfoldercombo",
+                name       : "sharedfolderref",
+                fieldLabel : _("Shared Folder"),
+                allowNone  : false,
+                plugins    : [{
+                    ptype : "fieldinfo",
+                    text  : _("This share will have ownership changed to www-data:users.")
                 }]
             }]
         }];
@@ -64,9 +64,9 @@ Ext.define("OMV.module.admin.service.webdav.Settings", {
 });
 
 OMV.WorkspaceManager.registerPanel({
-    id: "settings",
-    path: "/service/webdav",
-    text: _("Settings"),
-    position: 10,
-    className: "OMV.module.admin.service.webdav.Settings"
+    id        : "settings",
+    path      : "/service/webdav",
+    text      : _("Settings"),
+    position  : 10,
+    className : "OMV.module.admin.service.webdav.Settings"
 });
