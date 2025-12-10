7cd075fbf076cf20caf9a9f68c6dd8d1916f819f
Aaron Murray <plugins@omv-extras.org>
2025-12-10 22:22:03 +0000

    fix path
    Signed-off-by: Aaron Murray <plugins@omv-extras.org>

--- usr/share/openmediavault/confdb/migrations.d/conf.service.webdav_8.0.1.sh
+++ usr/share/openmediavault/confdb/migrations.d/conf.service.webdav_8.0.1.sh
@@ -4,12 +4,12 @@ set -e
 
 . /usr/share/openmediavault/scripts/helper-functions
 
-if ! omv_config_exists "/config/services/compose/path"; then
-  omv_config_add_key "/config/services/compose" "path" "webdav"
+if ! omv_config_exists "/config/services/webdav/path"; then
+  omv_config_add_key "/config/services/webdav" "path" "webdav"
 fi
 
-if ! omv_config_exists "/config/services/compose/userisolation"; then
-  omv_config_add_key "/config/services/compose" "userisolation" "0"
+if ! omv_config_exists "/config/services/webdav/userisolation"; then
+  omv_config_add_key "/config/services/webdav" "userisolation" "0"
 fi
 
 omv_module_set_dirty webdav
